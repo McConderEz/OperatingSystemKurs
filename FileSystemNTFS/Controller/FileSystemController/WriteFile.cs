0c2fe578acbeaaab530d07f22f2c9ccb8c09491e
McConderEz <kamazitun.jaykehd@gmail.com>
2024-04-02 01:37:59 +0300

    added all file operation, almost all dir operation, formatting and etc.

--- FileSystemNTFS/Controller/FileSystemController/WriteFile.cs
+++ FileSystemNTFS/Controller/FileSystemController/WriteFile.cs
@@ -24,10 +24,24 @@ namespace FileSystemNTFS.BL.Controller.FileSystemController
             {
                 using(var bw = new BinaryWriter(fs))
                 {
-                    byte[] dataBytes = Encoding.UTF8.GetBytes(data.ToString());
-                    long startIndex = bw.Seek((int)FileSystem.SuperblockController.Superblock.BitmapOffset, SeekOrigin.Begin);
-                    bw.Write(dataBytes, 0, dataBytes.Length);
-                    FileSystem.SuperblockController.Superblock.BitmapOffset = bw.Seek(0, SeekOrigin.End) + 1;
+                    if (FileSystem.SuperblockController.IsClusterFree(i) && dataSize <= 4096) // Запись данных в файл, размер которых не превышает размер одного кластера(4кб)
+                    {
+                        FileSystem.SuperblockController.MarkClusterAsUsed(dataBytes, i);
+                        mftItem.Attributes.IndexesOnClusterBitmap.Add(new Indexer(i, dataSize)); //В индекс будем записывать первое значение - индекс кластера, второе - фактический размер занятого кластера из 4 кб
+                        WriteBytesToFile(fullPath, stream, dataBytes, dataSize);
+                        break;
+                    }
+                    else if (FileSystem.SuperblockController.IsClusterFree(i) && dataSize > 4096) // Запись данных в файл, размер которых больше одного кластера(4кб)
+                    {
+                        FileSystem.SuperblockController.MarkClustersAsUsedForLargeFile(mftItem, dataBytes, dataSize, i);
+                        WriteBytesToFile(fullPath, stream, dataBytes, dataSize);
+                        break;
+                    }
+                }
+                mftItem.Attributes.Length = new FileInfo(fullPath).Length;
+                stream.Dispose();
+                stream.Close();
+            }
 
                     mftItem.Attributes.IndexesOnClusterBitmap.Add(new Indexer((int)startIndex, (int)(FileSystem.SuperblockController.Superblock.BitmapOffset - 1)));
                     mftItem.Attributes.Length = (ulong)(new FileInfo(fullPath)).Length;
